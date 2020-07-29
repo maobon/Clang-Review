bd5e2870c943e783d8b5e470b5521ec6fb8493b8
maobonhaha@gmail.com <880910abcD>
2020-07-29 11:06:38 +0800

    add

--- topic7.c
+++ topic7.c
@@ -5,27 +5,31 @@
 // 7. 输入年（year）、月（month）和日（day），计算并输出该日是该年的第几天（sumdays)
 
 #include <stdio.h>
-#include <time.h>
 
 int main() {
     puts("输入年（year）月（month）和日（day）");
 
-//    int year, month, day;
-//
-//    scanf("%d%d%d", &year, &month, &day);
-//     printf("%d年%d月%d日", year, month, day);
+    static int days_of_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-    struct tm *tm;
-    tm->tm_year = 2020;
-    tm->tm_mon = 7;
-    tm->tm_mday = 28;
+    int year, month, day;
+    scanf("%d%d%d", &year, &month, &day);
 
-    time_t tt = mktime(tm);
-    char *ret = ctime(tt);
+    int total_days = 0;
 
-    printf("%s", ret);
+    for (int i = 1; i < month; i++) {
+        int dom = days_of_month[i - 1];
+        printf("统计月份[%d]:%d\n", i, dom);
+        total_days += dom;
+    }
 
-    //mktime(tm);
+    if (year % 4 == 0 || year % 400 == 0) {
+        puts("是闰年 +1");
+        total_days += 1;
+    }
 
+    printf("当月已过天数:%d\n", day);
+    total_days += day;
+
+    printf("该日是该年的第几天:%d天", total_days);
     return 0;
 }
